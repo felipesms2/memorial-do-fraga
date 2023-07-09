b79d5d10b7fb09aeef6e1a6e43fb62c34964cc49
felipesms2 <felipesms@live.com>
2023-07-09 20:17:07 -0300

    Thumbnail table created

---
+++ app/Http/Controllers/ThumbnailController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreThumbnailRequest;
+use App\Http\Requests\UpdateThumbnailRequest;
+use App\Models\Thumbnail;
+
+class ThumbnailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreThumbnailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Thumbnail $thumbnail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Thumbnail $thumbnail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateThumbnailRequest $request, Thumbnail $thumbnail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Thumbnail $thumbnail)
+    {
+        //
+    }
+}
