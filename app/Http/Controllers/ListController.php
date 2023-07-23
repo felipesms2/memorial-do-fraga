a9c66206334359fc2f68c87377afaedd2d0fe15d
felipesms2 <felipesms@live.com>
2023-07-23 12:23:16 -0400

    refactoring some code

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -8,6 +8,7 @@ use App\Models\Video;
 use App\Models\Thumbnail;
 use Carbon\Carbon;
 use App\Traits\AdditionalVideoDataTrait;
+use App\Models\Tag;
 
 class ListController extends Controller
 {
@@ -18,168 +19,56 @@ class ListController extends Controller
 
     public function index()
     {
-        $count = 1;
         $fileToRead  = __DIR__ . "../../../../resources/video_list.csv";
         $file = fopen($fileToRead,"r");
-        $queryId ="";
-        $bunchID = [];
-
-        while(! feof($file))
-    {
-
-        if( $text =fgetcsv($file)){
-            $bunchID[] = $text[1];
-        }
-
-        if (1==25)
+        $bunchID = $this->setBunchId($file);
+        $group25 = array_chunk($bunchID, 25);
+        foreach ($group25 as $g)
         {
-        	$client = new Client();
-	    	$response = $client->get($endpoint);
-
-		$data = json_decode($response->getBody(),
-		false);
-
-
-$titles = [];
-foreach ($data->items as $item)
-{
-    $sni = $item->snippet;
-    // $table->string("title");
-    // $table->string("channelId");
-    // $table->string("kind");
-    // $table->string("etag");
-    // $table->string("video_id");
-    // $table->dateTime("publishedAt");
-    // $table->longText("description");
-    // $table->string("categoryId");
-    // $table->string("liveBroadcastContent");
-    // $table->string("defaultAudioLanguage");
-
-    $title = $sni->title;
-    $channelId = $sni->channelId;
-    $kind=$item->kind;
-    $etag=$item->etag;
-    $video_id=$item->id;
-    $publishedAt = Carbon::parse($sni->publishedAt, 'UTC');
-    $description = $sni->description;
-    $categoryId = $sni->categoryId  ;
-    $defaultAudioLanguage = $sni->defaultAudioLanguage;
-    $liveBroadcastContent = $sni->liveBroadcastContent;
-
-    $insertVideo = Video::Create(
-        [
-            "title" => $title,
-            "channelId" => $channelId,
-            "kind" => $kind,
-            "video_id" => $video_id,
-            "etag" => $etag,
-            "publishedAt" => $publishedAt,
-            "description" => $description,
-            "categoryId" => $categoryId,
-            "defaultAudioLanguage" => $defaultAudioLanguage,
-            "liveBroadcastContent" => $liveBroadcastContent,
-        ]
-    );
-
-    // return $insertVideo;
-
-}
-
-        }
-
-        // dd($bunchID);
-
-    }
-
-    /*
-    try {
-    $pdo = new PDO('sqlite:../../../database/database.sqlite');
-    echo "Conexão efetuada com sucesso.";
-} catch (PDOException $e) {
-    echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
-}*/
-  // Endpoint
-    /*   https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=fL5cWD84mYw&
-        id=XYe94GtMkJQ&
-        id=qzq2lJZBtCs&
-        id=lHlH94GklAE&
-        id=Mc4ByWixwdw&
-    key=AIzaSyBB93pQFssH3DTGXfROEojYCJARPOzQj04&id=GOuSNVTKfRs */
-fclose($file);
-    $group25 = array_chunk($bunchID, 25);
-    foreach ($group25 as $g)
-        {
-            foreach ($g as $u)
-            {
-                $queryId .= "&id=" . $u ;
-                $endpoint = "https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&" . $queryId .
-                "&key=AIzaSyBB93pQFssH3DTGXfROEojYCJARPOzQj04&id=GOuSNVTKfRs";
-            }
+            $endpoint = $this->setVideoQueryString($g);
             $client = new Client();
             $response = $client->get($endpoint);
             $data = json_decode($response->getBody());
-            $titles = [];
-foreach ($data->items as $item)
-{
+            foreach ($data->items as $item)
+            {
+                $sni = $item->snippet;
+                $sni->defaultAudioLanguage = "NONE";
+                $titles[]= $sni->title;
+                $title = $sni->title;
+                $channelId = $sni->channelId;
+                $kind=$item->kind;
+                $etag=$item->etag;
+                $video_id=$item->id;
+                $publishedAt = Carbon::parse($sni->publishedAt, 'UTC');
+                $description = $sni->description;
+                $categoryId = $sni->categoryId;
+                if(isset($sni->defaultAudioLanguage))
+                {
+                    $defaultAudioLanguage = $sni->defaultAudioLanguage;
+                }
+                $liveBroadcastContent = $sni->liveBroadcastContent;
 
-    $sni = $item->snippet;
-    // $table->string("title");
-    // $table->string("channelId");
-    // $table->string("kind");
-    // $table->string("etag");
-    // $table->string("video_id");
-    // $table->dateTime("publishedAt");
-    // $table->longText("description");
-    // $table->string("categoryId");
-    // $table->string("liveBroadcastContent");
-    // $table->string("defaultAudioLanguage");
+                $insertVideo = Video::Create(
+                    [
+                        "title" => $title,
+                        "channelId" => $channelId,
+                        "kind" => $kind,
+                        "video_id" => $video_id,
+                        "etag" => $etag,
+                        "publishedAt" => $publishedAt,
+                        "description" => $description,
+                        "categoryId" => $categoryId,
+                        "defaultAudioLanguage" => $defaultAudioLanguage,
+                        "liveBroadcastContent" => $liveBroadcastContent,
+                    ]
+                );
 
-    $sni->defaultAudioLanguage = "NONE";
-    $titles[]= $sni->title;
-    $title = $sni->title;
-    $channelId = $sni->channelId;
-    $kind=$item->kind;
-    $etag=$item->etag;
-    $video_id=$item->id;
-    $publishedAt = Carbon::parse($sni->publishedAt, 'UTC');
-    $description = $sni->description;
-    $categoryId = $sni->categoryId  ;
-    if(isset($sni->defaultAudioLanguage))
-    {
-        $defaultAudioLanguage = $sni->defaultAudioLanguage;
-    }
+                
 
-    $liveBroadcastContent = $sni->liveBroadcastContent;
-
-    $insertVideo = Video::Create(
-        [
-            "title" => $title,
-            "channelId" => $channelId,
-            "kind" => $kind,
-            "video_id" => $video_id,
-            "etag" => $etag,
-            "publishedAt" => $publishedAt,
-            "description" => $description,
-            "categoryId" => $categoryId,
-            "defaultAudioLanguage" => $defaultAudioLanguage,
-            "liveBroadcastContent" => $liveBroadcastContent,
-        ]
-    );
-    $thumbToInsert =$this->prepareThumb($item->snippet->thumbnails, $insertVideo->id);
-    foreach ($thumbToInsert as $tti)
-        {
-            $thumInserted[] = Thumbnail::create($tti);
-        }
+            }
 
         }
-
-        dump($titles);
-        $queryId = "";
-
     }
-}
-
-
 
     /**
      * Show the form for creating a new resource.
--- app/Traits/AdditionalVideoDataTrait.php
+++ app/Traits/AdditionalVideoDataTrait.php
@@ -18,5 +18,34 @@ trait AdditionalVideoDataTrait
             }
             return ($thumbToInsert);
         }
+
+        public function setVideoQueryString($unitGroup25)
+        {
+
+                $queryId = "";
+                foreach ($unitGroup25 as $u)
+                {
+                    $queryId .= "&id=" . $u ;
+                    $endpoint = "https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&key=AIzaSyBB93pQFssH3DTGXfROEojYCJARPOzQj04&" . $queryId .
+                    "";
+                }
+
+             return($endpoint);
+        }
+        public function setBunchId($csvFile)
+        {
+            $file = $csvFile;
+            $bunchID = [];
+            while(! feof($file))
+            {
+                if( $text =fgetcsv($file))
+                {
+                    $bunchID[] = $text[1];
+                }
+            }
+            fclose($file);
+            return $bunchID;
+
+        }
     }
 
